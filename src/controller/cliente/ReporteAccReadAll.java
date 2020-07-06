2f5ca459be40fa2f53fe054326f4a90004ef3728
toballatorre <tobal.latorre@gmail.com>
2020-07-06 13:04:48 -0400

    correccion errores conflicto

--- src/controller/cliente/ReporteAccReadAll.java
+++ src/controller/cliente/ReporteAccReadAll.java
@@ -1,69 +1,6 @@
-<<<<<<< HEAD
-package controller.cliente;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import dao.ReporteAccidenteDAO;
-import modelo.ReporteAccidenteDTO;
-
 /**
  * Servlet implementation class ReportesListar
  */
-<<<<<<< HEAD
-@WebServlet("/ReporteAccReadAll")
-=======
-@WebServlet("/ReportesAccReadAll")
->>>>>>> toballatorre05
-public class ReporteAccReadAll extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReporteAccReadAll() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		ReporteAccidenteDAO reportedao = new ReporteAccidenteDAO();
-		System.out.println(reportedao);
-		System.out.println(reportedao.toString());
-		List<ReporteAccidenteDTO> lreportes = new ArrayList<ReporteAccidenteDTO>();
-		int id = (int) request.getSession(false).getAttribute("idCliente");
-		lreportes = reportedao.readAllIdClient(id);
-		System.out.println(lreportes);
-		
-		request.setAttribute("listadoreportes", lreportes);
-		request.getRequestDispatcher("cliente/RegAccidentesClienteReadAll.jsp").forward(request, response);
-		
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
-}
-=======
 package controller.cliente;
 
 import java.io.IOException;
@@ -125,4 +62,3 @@ public class ReporteAccReadAll extends HttpServlet {
 	}
 
 }
->>>>>>> abfb3f6ee65889fb4803ce44ad163b93b9ddb1e5
